8ce62d1aa3df2cd6618bbd3f629de015eb13cbdf
Alex Khitry <khitrii03@gmail.com>
2023-06-08 10:04:05 +0300

    init: initialise project

---
+++ src/pages/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./app.scss";
+
+export const App: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <h1>Hello React</h1>;
+    </div>
+  );
+};
---
+++ webpack.config.cjs
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', 'jsx'],
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@contexts': path.resolve(__dirname, 'src/contexts'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@layouts': path.resolve(__dirname, 'src/layouts'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@routes': path.resolve(__dirname, 'src/routes'),
+            '@services': path.resolve(__dirname, 'src/services'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+          }),
+          new CopyPlugin({
+            patterns: [
+              { from: './src/assets', to: '' }, // <- your path to favicon
+            ],
+          }),
+    ],
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, './dist')
+        }
+    }
+}
